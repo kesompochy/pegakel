edd570b28d8b8e2c1fa65e352f9d121e7edbee50
kesompochy <kesompochy@gmail.com>
2024-03-12 22:51:50 +0900

    clickイベント

--- client/src/components/Canvas.vue
+++ client/src/components/Canvas.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 const canvasRef = ref<HTMLCanvasElement | null>(null);
+
+onMounted(() => {
+  const canvas = canvasRef.value;
+  if (canvas) {
+    canvas.onClick = (e) => {
+      console.log(e);
+    };
+    const ctx = canvas.getContext("2d");
+    if (ctx) {
+      ctx.fillStyle = "red";
+      ctx.fillRect(10, 10, 150, 100);
+    }
+  }
+});
 </script>
 
 <template>
--- client/tests/canvas.test.ts
+++ client/tests/canvas.test.ts
@@ -2,9 +2,18 @@ import Canvas from "~/components/Canvas.vue";
 import { mount } from "@vue/test-utils";
 
 describe("canvas", () => {
-  it("canvas", async () => {
+  it("exists", async () => {
     expect("Canvas").toBe(Canvas.__name)
     const wrapper = mount(Canvas);
     expect(wrapper.find("canvas").exists()).toBe(true);
+  });
+  it("has a canvas element", async () => {
+    const wrapper = mount(Canvas);
+    expect(wrapper.find("canvas").exists()).toBe(true);
+  });
+  it("dispatch a function when the canvas is clicked", async () => {
+    const wrapper = mount(Canvas);
+    wrapper.find("canvas").trigger("click");
+    expect(wrapper.emitted().click).toBeTruthy();
   });
 });
