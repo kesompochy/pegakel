4f184689f2602c1a38a5439102fe16db5577d6e7
kesompochy <kesompochy@gmail.com>
2024-03-12 22:43:14 +0900

    testをかく用意

--- client/tests/canvas.test.ts
+++ client/tests/canvas.test.ts
@@ -1,10 +1,10 @@
 import Canvas from "~/components/Canvas.vue";
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 
 describe("canvas", () => {
   it("canvas", async () => {
     expect("Canvas").toBe(Canvas.__name)
-    const wrapper = shallowMount(Canvas);
-    expect(wrapper).toBeTruthy();
+    const wrapper = mount(Canvas);
+    expect(wrapper.find("canvas").exists()).toBe(true);
   });
 });
