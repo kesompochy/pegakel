04aaa2d3cd7c4a07ae9b0c24e9a8580ee498f3a0
kesompochy <kesompochy@gmail.com>
2024-03-27 08:46:21 +0900

    biomeをコミットしておく

--- client/tests/core/Sheet.test.ts
+++ client/tests/core/Sheet.test.ts
@@ -1,12 +1,12 @@
-import Sheet from '~/core/Sheet';
+import Sheet from "~/core/Sheet";
 
-describe ('Sheet', () => {
+describe("Sheet", () => {
   it("should have name", () => {
-    const sheet = new Sheet('test');
-    expect(sheet.name).toBe('test');
+    const sheet = new Sheet("test");
+    expect(sheet.name).toBe("test");
   });
   it("should have sprites", () => {
-    const sheet = new Sheet('test');
+    const sheet = new Sheet("test");
     expect(sheet.sprites).toEqual([]);
   });
 });
