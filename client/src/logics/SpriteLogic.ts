775cbd1e99b50131f0ec89b65ce9e3cbdb7f9e31
kesompochy <kesompochy@gmail.com>
2024-08-25 17:09:13 +0900

    sprite resize

--- client/src/logics/SpriteLogic.ts
+++ client/src/logics/SpriteLogic.ts
@@ -14,5 +14,33 @@ export default {
   changeClip(sprite: Sprite, clip: { x: number, y: number }) {
     sprite.clipX = clip.x;
     sprite.clipY = clip.y;
+  },
+  updateSpriteSize(sprite: Sprite, left: number, top: number, bottom: number, right: number) {
+    const newWidth = sprite.width + left + right;
+    const newHeight = sprite.height + top + bottom;
+    if ( left > 0) {
+      sprite.pixels = sprite.pixels.map(row => [...Array(left).fill(new ColorState(0, 0, 0, 0)), ...row]);
+    } else if (left < 0) {
+      sprite.pixels = sprite.pixels.map(row => row.slice(-left));
+    }
+    if (right > 0) {
+      sprite.pixels = sprite.pixels.map(row => [...row, ...Array(right).fill(new ColorState(0, 0, 0, 0))]);
+    } else if (right < 0) {
+      sprite.pixels = sprite.pixels.map(row => row.slice(0, right));
+    }
+    if (top > 0) {
+      const newRows = Array(top).fill(new ColorState(0, 0, 0, 0)).map(() => Array(newWidth).fill(new ColorState(0, 0, 0, 0)));
+      sprite.pixels = [...newRows, ...sprite.pixels];
+    } else if (top < 0) {
+      sprite.pixels = sprite.pixels.slice(-top);
+    }
+    if (bottom > 0) {
+      const newRows = Array(bottom).fill(new ColorState(0, 0, 0, 0)).map(() => Array(newWidth).fill(new ColorState(0, 0, 0, 0)));
+      sprite.pixels = [...sprite.pixels, ...newRows];
+    } else if (bottom < 0) {
+      sprite.pixels = sprite.pixels.slice(0, bottom);
+    }
+    sprite.width = newWidth;
+    sprite.height = newHeight;
   }
 }
--- client/tests/logics/SpriteLogic.test.ts
+++ client/tests/logics/SpriteLogic.test.ts
@@ -1,5 +1,6 @@
 import SpriteLogic from '~/logics/SpriteLogic';
 import ColorState from '~/core/ColorState';
+import Sprite from '~/core/Sprite';
 
 describe('SpriteLogic', () => {
   it("should create a new sprite", () => {
@@ -23,5 +24,44 @@ describe('SpriteLogic', () => {
     SpriteLogic.changeClip(sprite, { x: 5, y: 7 });
     expect(sprite.clipX).toBe(5);
     expect(sprite.clipY).toBe(7);
+  });
+  describe("update sprite size", () => {
+    let sprite: Sprite;
+    beforeEach(() => {
+      sprite = SpriteLogic.createSprite(10, 10);
+      sprite.pixels[0][0] = new ColorState(100, 0, 0, 1);
+      sprite.pixels[9][9] = new ColorState(0, 100, 0, 1);
+      sprite.pixels[0][9] = new ColorState(0, 0, 100, 1);
+      sprite.pixels[9][0] = new ColorState(5, 0, 0, 1);  
+    });
+    it("should update with no change", () => {
+      SpriteLogic.updateSpriteSize(sprite, 0, 0, 0, 0);
+      expect(sprite.width).toBe(10);
+      expect(sprite.height).toBe(10);
+      expect(sprite.pixels[0][0]).toStrictEqual(new ColorState(100, 0, 0, 1));
+      expect(sprite.pixels[9][9]).toStrictEqual(new ColorState(0, 100, 0, 1));
+      expect(sprite.pixels[0][9]).toStrictEqual(new ColorState(0, 0, 100, 1));
+      expect(sprite.pixels[9][0]).toStrictEqual(new ColorState(5, 0, 0, 1));
+    });
+    it("should update sprite to be larger", () => {
+      SpriteLogic.updateSpriteSize(sprite, 1, 1, 1, 1);
+      expect(sprite.width).toBe(12);
+      expect(sprite.height).toBe(12);
+      expect(sprite.pixels[0][0]).toStrictEqual(new ColorState(0, 0, 0, 0));
+      expect(sprite.pixels[11][11]).toStrictEqual(new ColorState(0, 0, 0, 0));
+      expect(sprite.pixels[1][1]).toStrictEqual(new ColorState(100, 0, 0, 1));
+      expect(sprite.pixels[10][10]).toStrictEqual(new ColorState(0, 100, 0, 1));
+      expect(sprite.pixels[1][10]).toStrictEqual(new ColorState(0, 0, 100, 1));
+      expect(sprite.pixels[10][1]).toStrictEqual(new ColorState(5, 0, 0, 1));
+    });
+    it("should update sprite to be smaller", () => {
+      SpriteLogic.updateSpriteSize(sprite, -1, -1, -1, -1);
+      expect(sprite.width).toBe(8);
+      expect(sprite.height).toBe(8);
+      expect(sprite.pixels[0][0]).toStrictEqual(new ColorState(0, 0, 0, 0));
+      expect(sprite.pixels[7][7]).toStrictEqual(new ColorState(0, 0, 0, 0));
+      expect(sprite.pixels[0][7]).toStrictEqual(new ColorState(0, 0, 0, 0));
+      expect(sprite.pixels[7][0]).toStrictEqual(new ColorState(0, 0, 0, 0));
+    });
   });
 });
